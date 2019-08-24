d27870f0ce1dc5023516c0481df803ce54fccbaa
zhangquan17660 <981944726@qq.com>
2019-08-24 19:26:30 +0800

    study first commit by zq

---
+++ study/src/main/java/com/zq/App.java
@@ -0,0 +1,10 @@
+package com.zq;
+
+public class App {
+    public static void main(String[] args) {
+        App01 app = new App01();
+        System.out.println(app.getAppName());
+    }
+
+
+}
---
+++ studymapper/src/main/java/com/zq/App01.java
@@ -0,0 +1,7 @@
+package com.zq;
+
+public class App01 {
+    public  String getAppName(){
+        return "zq";
+    }
+}
