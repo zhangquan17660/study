82aff3a8d3feebf096fc97166f80f3a8214d0f2c
zhangquan17660 <981944726@qq.com>
2019-09-18 22:45:00 +0800

    mvc01

--- springmvc001/src/main/java/com/zq/mvc/controller/HelloController.java
+++ springmvc001/src/main/java/com/zq/mvc/controller/HelloController.java
@@ -1,10 +1,8 @@
 package com.zq.mvc.controller;
 
+import com.zq.mvc.pojo.Account;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.nio.file.Path;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/test")
@@ -12,6 +10,32 @@ public class HelloController {
     @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
     public String hello(){
         System.out.println("hello");
+        return "success";
+    }
+    //表单属性和bean对应
+    @RequestMapping(value = "/hello/account" ,method = RequestMethod.POST)
+    public String helloAccount(Account account){
+        System.out.println("hello");
+        System.out.println(account.getPassword());
+        return "success";
+    }
+    //@RequestParam和传递参数对应，后端参数可以随便定义
+    @RequestMapping(value = "/hello/param" ,method = RequestMethod.GET)
+    public String helloAccount(@RequestParam("para") String par){
+        System.out.println("hello");
+        System.out.println(par);
+        return "success";
+    }
+    //@RequestBody表示想获取表单内容，提交方式只能是POST获取格式id=111&username=11&password=11
+    @RequestMapping(value = "/hello/body" ,method = RequestMethod.POST)
+    public String helloBody(@RequestBody String body){
+        System.out.println(body);
+        return "success";
+    }
+//使用@PathVariable获取url上的参数，适用restful风格的请求，即路径前缀相同，根据请求方式和参数形式进行路径匹配
+    @RequestMapping(value = "/hello/param/{id}" ,method = RequestMethod.GET)
+    public String helloVariable(@PathVariable("id") String id){
+        System.out.println(id);
         return "success";
     }
 }
--- springmvc001/src/main/webapp/index.jsp
+++ springmvc001/src/main/webapp/index.jsp
@@ -2,5 +2,20 @@
 <body>
 <h2>Hello World!</h2>
 <a href="test/hello">ddd</a>
+<form action="test/hello/account" method="post">
+     <input name="id" type="text">
+    <input name="username" type="text">
+    <input name="password" type="text">
+    <input type="submit" name="提交" >
+</form>
+
+<form action="test/hello/body" method="post">
+    <input name="id" type="text">
+    <input name="username" type="text">
+    <input name="password" type="text">
+    <input type="submit" name="body" >
+</form>
+<a href="test/hello/param?para=111">ssss</a>
+<a href="test/hello/param/111">vvvvv</a>
 </body>
 </html>
