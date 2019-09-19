113602bcabddbfaf26fa42fe4e1c641a9e2cb647
zhangquan17660 <zhangquan17660@talkweb.com.cn>
2019-09-19 09:15:46 +0800

    qq

--- studyutil/src/main/java/com/zq/jdbc/service/impl/AccountService.java
+++ studyutil/src/main/java/com/zq/jdbc/service/impl/AccountService.java
@@ -5,8 +5,11 @@ import com.zq.jdbc.pojo.Account;
 import com.zq.jdbc.service.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS ,readOnly = true)
 public class AccountService implements IAccountService {
     @Autowired
     private Idao idao;
@@ -26,6 +29,7 @@ public class AccountService implements IAccountService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED ,readOnly = false)
     public void transfer(int sourceName, int targetName, float money) {
         System.out.println("start transfer.....");
         //根据id找出转账户出
@@ -35,6 +39,7 @@ public class AccountService implements IAccountService {
         //转出账户-100
         accountSource.setMoney(accountSource.getMoney() - 100f);
         //转入账户+100
+        int i  = 1/0;
         accountTarget.setMoney(accountTarget.getMoney()+100f);
         //更新账户
         idao.updateAccount(accountSource);
